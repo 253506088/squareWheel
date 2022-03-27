ff56fb174ca28715d65d9ad9c300d16dec4f21ae
韭香不怕巷子深 <10355512+jxbpxzs@user.noreply.gitee.com>
2022-03-27 13:35:06 +0800

    初次提交

---
+++ MyHashMap/src/main/java/com/example/myhashmap/MyHashMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHashMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHashMapApplication.class, args);
+    }
+
+}
