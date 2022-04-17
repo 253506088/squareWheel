f8feeb70140fe656d07dca1ec30c2deeb3ea33e2
韭香不怕巷子深 <10355512+jxbpxzs@user.noreply.gitee.com>
2022-04-17 20:03:25 +0800

    原味版本的Quartz的运行案例

--- mtQuartz/src/main/java/com/example/mtquartz/quartzOriginally/MyJobOriginally.java
+++ mtQuartz/src/main/java/com/example/mtquartz/quartzOriginally/MyJobOriginally.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
+import org.quartz.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,13 +13,14 @@ import org.slf4j.LoggerFactory;
  */
 public class MyJobOriginally implements Job {
     private Logger logger = LoggerFactory.getLogger(getClass());
+    public static int count = 0;
 
     @SneakyThrows
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         //获取在【创建JobDetail、创建Trigger】时传入的【JobDataMap】，这里相同的key也会被覆盖
         JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
-        if(false){
+        if (false) {
             //只获取【创建JobDetail】时传入的【JobDataMap】
             jobExecutionContext.getJobDetail().getJobDataMap();
             //只获取【创建Trigger】时传入的【JobDataMap】
@@ -34,5 +36,12 @@ public class MyJobOriginally implements Job {
         );
         //对于quartz来说，哪怕【任务的执行时间】大于【任务执行间隔】也是无所谓的
         Thread.sleep(2000);
+        count++;
+        if (count == 3) {
+            Scheduler scheduler = jobExecutionContext.getScheduler();
+            scheduler.deleteJob(jobExecutionContext.getJobDetail().getKey());
+//            scheduler.shutdown();
+            logger.error("满{}次，任务结束", count);
+        }
     }
 }
