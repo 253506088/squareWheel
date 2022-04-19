aa12a656aa631e9779446bb5fa7186d742c5fce8
韭香不怕巷子深 <10355512+jxbpxzs@user.noreply.gitee.com>
2022-04-19 21:43:40 +0800

    原味版本的Quartz的运行案例

--- mtQuartz/src/main/java/com/example/mtquartz/quartzOriginally/MyJobOriginally.java
+++ mtQuartz/src/main/java/com/example/mtquartz/quartzOriginally/MyJobOriginally.java
@@ -1,16 +1,16 @@
 package com.example.mtquartz.quartzOriginally;
 
 import lombok.SneakyThrows;
-import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
-import org.quartz.Scheduler;
+import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 原味版本的Quartz的job实现类，并非是spring boot版本的
+ * @DisallowConcurrentExecution，意味着：quartz每一次执行都是同一个的Job实现类对象
+ * 在默认情况下，quartz每一次执行都会创建一个新的Job实现类对象
  */
+//@DisallowConcurrentExecution
 public class MyJobOriginally implements Job {
     private Logger logger = LoggerFactory.getLogger(getClass());
     public static int count = 0;
@@ -35,13 +35,32 @@ public class MyJobOriginally implements Job {
                 Thread.currentThread().getId()
         );
         //对于quartz来说，哪怕【任务的执行时间】大于【任务执行间隔】也是无所谓的
-        Thread.sleep(2000);
+        Thread.sleep(6000);
         count++;
+
         if (count == 3) {
+            //根据key删除掉已经执行过三遍的任务
             Scheduler scheduler = jobExecutionContext.getScheduler();
             scheduler.deleteJob(jobExecutionContext.getJobDetail().getKey());
-//            scheduler.shutdown();
-            logger.error("满{}次，任务结束", count);
+            logger.error("满{}次，任务结束，新任务开始", count);
+
+            count = 0;
+            //第一步，创建JobDetail，用于指定具体要执行什么
+            JobDetail jobDetail = JobBuilder.newJob(MyJobOriginally.class).
+                    withIdentity("开始亏钱", "股市").
+                    build();
+            //第二步，创建Trigger，用于指定在何时执行
+            Trigger trigger = TriggerBuilder.newTrigger().
+                    withIdentity("9.30", "开盘").
+                    usingJobData("myName", "韭菜").
+                    usingJobData("myHome", "A股").
+                    startNow().//立马就启动的任务
+                    withSchedule(
+                    //之后3秒一次
+                    CronScheduleBuilder.cronSchedule("*/3 * * * * ?")
+            ).build();
+            //第三步，Scheduler调度器，用于调度定时任务
+            scheduler.scheduleJob(jobDetail, trigger);
         }
     }
 }
