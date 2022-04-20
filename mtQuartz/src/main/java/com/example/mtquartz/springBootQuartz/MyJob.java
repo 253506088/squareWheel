222769d9fde27246c66fc36c069cfa32e05376f5
253506088 <2842513372@qq.com>
2022-04-20 17:59:27 +0800

    quartz

---
+++ mtQuartz/src/main/java/com/example/mtquartz/springBootQuartz/MyJob.java
@@ -0,0 +1,32 @@
+package com.example.mtquartz.springBootQuartz;
+
+import lombok.SneakyThrows;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.PersistJobDataAfterExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/**
+ * Spring Boot的写法，继承QuartzJobBean类
+ */
+@PersistJobDataAfterExecution
+public class MyJob extends QuartzJobBean {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @SneakyThrows
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) {
+        JobDataMap jobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+        int count = jobDataMap.getInt("count");
+        count++;
+        logger.error("任务【{}】第【{}】次执行，线程id【{}】",
+                jobDataMap.get("taskName"),
+                count,
+                Thread.currentThread().getId()
+        );
+        jobDataMap.put("count",count);
+        Thread.sleep(1000);
+    }
+}
---
+++ mtQuartz/src/main/java/com/example/mtquartz/springBootQuartz/MyQuartzConfig.java
@@ -0,0 +1,42 @@
+package com.example.mtquartz.springBootQuartz;
+
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringBoot整合Quartz的关键就在于这个配置文件
+ * 只需要创建JobDetail和Trigger，无需Scheduler
+ */
+@Configuration
+public class MyQuartzConfig {
+    /**
+     * 注册一个JobDetail的bean
+     *
+     * @return
+     */
+    @Bean
+    public JobDetail myJob001() {
+        return JobBuilder.newJob(MyJob.class)//MyJob我们的业务类
+                .withIdentity("MyJob-001")//可以给该JobDetail起一个id
+                .usingJobData("taskName", "考公吧-001")
+                .usingJobData("count", 0)
+                .storeDurably()//即使没有Trigger关联时，也不需要删除该JobDetail
+                .build();
+    }
+
+    /**
+     * 注册一个Trigger的bean
+     * @return
+     */
+    @Bean
+    public Trigger myJob001Trigger() {
+        //5秒一次
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("*/5 * * * * ?");
+        return TriggerBuilder.newTrigger()
+                .forJob(this.myJob001())//关联上述的JobDetail
+                .withIdentity("myJob001Trigger")//给Trigger起个id
+                .withSchedule(cronScheduleBuilder)
+                .build();
+    }
+}
