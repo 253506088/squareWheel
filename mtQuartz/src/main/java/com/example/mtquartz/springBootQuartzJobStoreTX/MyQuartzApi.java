2ec09b361b93df6e0835b97be2b4c07a8bb0d5f9
韭香不怕巷子深 <10355512+jxbpxzs@user.noreply.gitee.com>
2022-04-24 22:54:46 +0800

    修改

--- mtQuartz/src/main/java/com/example/mtquartz/springBootQuartzJobStoreTX/MyQuartzApi.java
+++ mtQuartz/src/main/java/com/example/mtquartz/springBootQuartzJobStoreTX/MyQuartzApi.java
@@ -10,14 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyQuartzApi {
 
+
     @GetMapping("/add")
-    public void add(){
+    public void add() {
         JobDataMap jobDataMap = new JobDataMap();
-        jobDataMap.put("taskName","api启动的任务");
-        jobDataMap.put("count",0);
-        QuartzUtils.addJob("JobTest",
+        jobDataMap.put("taskName", "api启动的任务");
+        jobDataMap.put("count", 0);
+        QuartzUtils.addJob("JobTest2",
                 "jobGroup",
-                "triggerTest",
+                "triggerTest2",
                 "triggerGroup",
                 MyJob.class,
                 "*/3 * * * * ?",
@@ -25,18 +26,34 @@ public class MyQuartzApi {
     }
 
     /**
-     * 启动所有定时任务
+     * 恢复任务
      */
-    @GetMapping("/shutdownJobs")
-    public void shutdownJobs(){
-        QuartzUtils.shutdownJobs();
+    @GetMapping("/resumeJob")
+    public void resumeJob() {
+        QuartzUtils.resumeJob("JobTest2", "jobGroup");
+    }
+
+    /**
+     * 暂停任务
+     */
+    @GetMapping("/pauseJob")
+    public void pauseJob() {
+        QuartzUtils.pauseJob("JobTest2", "jobGroup");
     }
 
     /**
      * 关闭所有定时任务
      */
+    @GetMapping("/shutdownJobs")
+    public void shutdownJobs() {
+        QuartzUtils.shutdownJobs();
+    }
+
+    /**
+     * 启动所有定时任务
+     */
     @GetMapping("/startJobs")
-    public void startJobs(){
+    public void startJobs() {
         QuartzUtils.startJobs();
     }
 }
